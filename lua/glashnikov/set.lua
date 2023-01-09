ca457e35a8ca458c1bcfd5c46ff3b502fdbe8b66
Anis Chahed <anis.chahed@swan.io>
2023-01-09 09:15:28 +0100

    change tab width to 2

--- lua/glashnikov/set.lua
+++ lua/glashnikov/set.lua
@@ -5,7 +5,7 @@ vim.opt.relativenumber = true
 -- indentation
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 4
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
