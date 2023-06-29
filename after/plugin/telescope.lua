2f7f0dcaab0e2124b36702eda9180642fb7d2225
HarissaHero <anis.chahed@aleriaconsulting.eu>
2023-06-29 17:23:19 +0200

    add grep word under cursor

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -5,7 +5,6 @@ vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fw', ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
-vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
 
 require('telescope').load_extension('fzf')
 
@@ -18,3 +17,6 @@ require('telescope').setup {
     }
   }
 }
+
+local live_grep_args_shortcuts = require('telescope-live-grep-args.shortcuts')
+vim.keymap.set('n', '<leader>fs', live_grep_args_shortcuts.grep_word_under_cursor) 
